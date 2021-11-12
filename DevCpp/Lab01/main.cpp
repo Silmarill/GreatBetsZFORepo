a107777fee6474fea2535cf2264487a05522e89a
mrsilmar <mrsilmar@gmail.com>
2021-11-12 16:49:09 +0700

    lab 01 DevCPP

---
+++ DevCpp/Lab01/main.cpp
@@ -0,0 +1,16 @@
+/******************************************
+ * Burmin Leonid                          *
+ * Linear                                 *
+ * Variant 1                              * 
+ ******************************************/
+ 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 7;
+  cout << "Hello World " << a;
+  return 0;
+}
+
