f420a73f13354aaeb75ac593c327a38f3d1ed042
mrsilmar <mrsilmar@gmail.com>
2021-11-12 16:44:42 +0700

    added first lab

---
+++ lab01.cpp
@@ -0,0 +1,17 @@
+/******************************************
+ * Бурмин Леонид                          *
+ * Программирование линейных алгоритмов   *
+ * Вариат 1                               * 
+ * Codeguide: ...                         
+ * https://onlinegdb.com/-PaOPZo47        *
+ ******************************************/
+ 
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a = 7;
+  cout << "Hello World " << a;
+  return 0;
+}
